d6c6040819e882730d6efbec1d1ca44378a1fc33
Siriki-Saikiran <sirikisaikiran@gmail.com>
2017-06-28 01:29:10 +0530

    Added a bunch of animations

---
+++ myscript.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    
+   
+   $(".hel").hover(function(){
+       $(this).addClass("animated pulse");
+       
+   });
+   
+    
+    
+    
+   
+    
+    
+    
+});
