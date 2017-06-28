1206461007bed99a127669a8dd5690d679fbbd13
Siriki-Saikiran <sirikisaikiran@gmail.com>
2017-06-28 16:15:30 +0530

    Added more cool animations and made the page lively

--- myscript.js
+++ myscript.js
@@ -1,8 +1,24 @@
+
 $(document).ready(function(){
     
    
-   $(".hel").hover(function(){
-       $(this).addClass("animated pulse");
+   $(".page2").hover(function(){
+       $(".hel").addClass("animated pulse");
+       
+   });
+   $(".page3").hover(function(){
+       $(".c").addClass("animated bounceInDown").html("<h3 align='center'>C</h3><img class='img-responsive img-size' src='images/c.png' >");
+        $(".cpp").addClass("animated bounceInDown").html("<h3 align='center'>C++</h3><img class='img-responsive img-size'src='images/c++.png'>");
+         $(".py").addClass("animated bounceInDown").html("<h3 align='center'>Python</h3><img class='img-responsive img-size'src='images/python.png'>");
+          $(".html").addClass("animated bounceInDown").html("<h3 align='center'>HTML 5</h3><img class='img-responsive img-size'src='images/html.png'>");
+           $(".css").addClass("animated bounceInDown").html("<h3 align='center'>CSS 3</h3><img class='img-responsive img-size'src='images/css.png'>");
+            $(".js").addClass("animated bounceInDown").html("<h3 align='center'>Javascript</h3><img class='img-responsive img-size'src='images/javascript.png'>");
+            $(".jq").addClass("animated bounceInUp").html("<h3 align='center'>jQuery</h3><img class='img-responsive img-size'src='images/jquery.png'>");
+           $(".bs").addClass("animated bounceInUp").html("<h3 align='center'>Bootstrap</h3><img class='img-responsive img-size'src='images/bootstrap.png'>");
+            $(".git").addClass("animated bounceInUp").html("<h3 align='center'>Git</h3><img class='img-responsive img-size'src='images/git.png'>");
+       
+       
+       
        
    });
    
